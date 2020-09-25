4940f4ec3846190e72f2307cb5721953ff390be1
Patrick Hannan <patrick@hannan.com>
2020-09-24 22:22:28 -0400

    Added markdown file format with user input dynamically added in.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,57 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
+function generateMarkdown(answers) {
   return `# ${answers.title}
 
-`;
+  ## Description 
+  
+  ${answers.description}
+  
+  
+  ## Table of Contents 
+  
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Credits](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  
+  ## Installation
+  
+  ${answers.installation}
+  
+  
+  ## Usage 
+  
+  ${answers.usage}
+  
+
+  ## License
+  
+  ${answers.license}
+  
+  
+  ## Contributing
+  
+  ${answers.contribution}
+  
+
+  ## Tests
+  
+  ${answers.tests}
+
+
+  ## Questions  
+
+  To view my other repositories along with my profile, you can visit my github below:
+
+  * [My Github](https://https://github.com/${answers.github})
+  
+  If you have any further questions, you can reach me at the email address below:
+
+  ${answers.email}
+  `;
 }
 
 module.exports = generateMarkdown;
